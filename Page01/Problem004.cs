275065adb030e3a0839e4955cfbd74be8ec860b6
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-05 16:57:15 -0400

    Problem 4 - complete

---
+++ Page01/Problem004.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ProjectEulerSharp.Page01
+{
+    /**************************************************************************
+     * URL: https://projecteuler.net/problem=4
+     * A palindromic number reads the same both ways. The largest palindrome 
+     * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+     * 
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+     **************************************************************************/
+    [TestClass]
+    public class Problem004 : ProblemBase
+    {
+        protected override long ExpectedAnswer => 906609;
+
+        protected override long SolutionImplementation()
+        {
+            long largest = 0;
+            foreach (var i in Enumerable.Range(100, 900))
+                foreach (var j in Enumerable.Range(i, 1000 - i))
+                    if (IsPalindrome(i * j) && i * j > largest)
+                        largest = i * j;
+            return largest;
+        }
+
+        private bool IsPalindrome(long value)
+        {
+            var digits = value.ToString().ToCharArray();
+            var length = digits.Length;
+            var midpoint = length / 2;
+            for (int i = 0; i <= midpoint; ++i)
+                if (digits[i] != digits[length - i - 1])
+                    return false;
+            return true;
+        }
+    }
+}
