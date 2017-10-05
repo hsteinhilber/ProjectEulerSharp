2cfaa7942e4f727e07fec2a30cdf870984f420b5
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-05 13:28:13 -0400

    Problem 2 - complete

---
+++ Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSharp
+{
+    public class Fibonacci : IEnumerable<int>
+    {
+        int first = 1, second = 2;
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            yield return first;
+            yield return second;
+            while (true)
+            {
+                var next = first + second;
+                first = second;
+                second = next;
+                yield return next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<int>)this).GetEnumerator();
+        }
+    }
+}
