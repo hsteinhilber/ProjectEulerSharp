dcfcb0106fafd4528c09da3323dc2f77cc4e5776
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-05 13:12:08 -0400

    Problem 1 - complete

--- Page01/Problem001.cs
+++ Page01/Problem001.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace ProjectEulerSharp
+namespace ProjectEulerSharp.Page01
 {
+    /**************************************************************************
+     * URL: https://projecteuler.net/problem=1
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+     * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+     * 
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     **************************************************************************/
     [TestClass]
     public class Problem001 : ProblemBase
     {
         [TestMethod]
         public void Solve()
         {
-            LogResult(42);
+            int result = Enumerable.Range(1, 999).Where(x => ((x % 3 == 0) || (x % 5 == 0))).Sum();
+
+            LogResult(result);
         }
 
 
--- ProblemBase.cs
+++ ProblemBase.cs
@@ -14,6 +14,22 @@ namespace ProjectEulerSharp
         {
         }
 
+        protected virtual void LogValue<T>(T value)
+        {
+            Logger.LogMessage("Value: {0}", value);
+        }
+
+        protected virtual void LogValue<T, U>(T values) where T: IEnumerable<U>
+        {
+            var text = new StringBuilder("[");
+            foreach (U value in values)
+                text.AppendFormat("{0}, ", value);
+            if (values.Any())
+                text.Remove(text.Length - 2, 2);
+            text.Append("]");
+            Logger.LogMessage("Collection: {0}", text);
+        }
+
         protected virtual void LogResult<T>(T result)
         {
             Logger.LogMessage("Test Result: {0}", result);
