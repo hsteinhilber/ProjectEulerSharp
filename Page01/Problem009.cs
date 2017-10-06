0b678e623ad46e7b164d72ca356065f8f4dcd3a0
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-06 08:50:52 -0400

    Problem 9 - complete

---
+++ Page01/Problem009.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerSharp.Page01
+{
+    /**************************************************************************
+     * URL: https://projecteuler.net/problem=9
+     * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+     * 
+     * a^2 + b^2 = c^2
+     * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+     * 
+     * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+     * Find the product abc.
+     **************************************************************************/
+    [TestClass]
+    public class Problem009 : ProblemBase
+    {
+        protected override long ExpectedAnswer => 31875000;
+
+        protected override long SolutionImplementation()
+        {
+            for (int a = 1; a <=1000; ++a)
+            {
+                for (int b = a + 1; b <= 1000; ++b)
+                {
+                    int c = 1000 - (a + b);
+                    if (c <= b) break;
+
+                    if (IsPythagoreanTriple(a, b, c))
+                        return a * b * c;
+                }
+            }
+            return 0;
+        }
+
+        bool IsPythagoreanTriple(int a, int b, int c)
+        {
+            if (a >= b) return false;
+            if (b >= c) return false;
+
+            return a * a + b * b == c * c;
+        }
+    }
+}
