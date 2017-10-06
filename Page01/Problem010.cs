8083f441da34418579e5f81da6c032c9ab6b2ee3
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-06 09:32:00 -0400

    Problem 10 - complete

---
+++ Page01/Problem010.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerSharp.Sequences;
+using System.Linq;
+
+namespace ProjectEulerSharp.Page01
+{
+    /**************************************************************************
+     * URL: https://projecteuler.net/problem=10
+     * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+     * 
+     * Find the sum of all the primes below two million.
+     **************************************************************************/
+    [TestClass]
+    public class Problem010 : ProblemBase
+    {
+        protected override long ExpectedAnswer => 142913828922;
+
+        protected override long SolutionImplementation()
+        {
+            return Primes.All.TakeWhile(x => x < 2000000).Sum();
+        }
+    }
+}
