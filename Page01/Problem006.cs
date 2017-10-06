3b374cdc5dd1cdf9990f0407aa01212e094088d9
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-05 23:16:51 -0400

    Problem 6 - complete

---
+++ Page01/Problem006.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ProjectEulerSharp.Page01
+{
+    /**************************************************************************
+     * URL: https://projecteuler.net/problem=6
+     * The sum of the squares of the first ten natural numbers is,
+     *     1^2 + 2^2 + ... + 10^2 = 385
+     *
+     * The square of the sum of the first ten natural numbers is,
+     *     (1 + 2 + ... + 10)^2 = 55^2 = 3025
+     *
+     * Hence the difference between the sum of the squares of the first ten 
+     * natural numbers and the square of the sum is 3025 − 385 = 2640.
+     * 
+     * Find the difference between the sum of the squares of the first one 
+     * hundred natural numbers and the square of the sum.
+     **************************************************************************/
+    [TestClass]
+    public class Problem006 : ProblemBase
+    {
+        protected override long ExpectedAnswer => 25164150;
+
+        protected override long SolutionImplementation()
+        {
+            var sumOfSquares = Enumerable.Range(1, 100).Select(x => x * x).Sum();
+            var sum = Enumerable.Range(1, 100).Sum();
+            var squareOfSum = sum * sum;
+            return squareOfSum - sumOfSquares;
+        }
+    }
+}
