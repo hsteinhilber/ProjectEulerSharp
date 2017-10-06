891c067eed34245dc7831287fec14f4d99e3fb11
Harry Steinhilber <harry.steinhilber@live.com>
2017-10-05 20:59:53 -0400

    Problem 5 - Complete

--- Page01/Problem005.cs
+++ Page01/Problem005.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using ProjectEulerSharp.Sequences;
+using System.Linq;
 
 namespace ProjectEulerSharp.Page01
 {
@@ -15,27 +17,41 @@ namespace ProjectEulerSharp.Page01
     [TestClass]
     public class Problem005 : ProblemBase
     {
+        protected override long ExpectedAnswer => 232792560;
+
         protected override long SolutionImplementation()
         {
             var resultFactors = new Dictionary<long, int>();
 
-            // For each number i => 1..20
-            //   Get the prime factors of i
-            //   For each prime factor
-            //   if result factors contains prime
-            //      if count(prime) in prime factors of i > count(prime) in factors of result
-            //         set count(prime) in factors of result = count(prime) in prime factors of i
-            //   else
-            //      add count(prime) in prime factors of i to factors of result
-            // result = sum(prime * count(prime)) for each factor of result
-
-            return 0;
+            foreach (var num in Enumerable.Range(1, 20))
+            {
+                var factors = GetPrimeFactors(num);
+                foreach (var prime in factors.Keys)
+                {
+                    if (!resultFactors.ContainsKey(prime))
+                        resultFactors[prime] = 0;
+                    resultFactors[prime] = Math.Max(resultFactors[prime], factors[prime]);
+                }
+            }
+            return (long)resultFactors.Aggregate(1.0d, (current, factor) => current * Math.Pow(factor.Key, factor.Value));
         }
 
         private IDictionary<long, int> GetPrimeFactors(long value)
         {
             var factors = new Dictionary<long, int>();
+            var current = value;
 
+            foreach (var prime in Primes.All.TakeWhile(x => x <= value))
+            {
+                factors[prime] = 0;
+                while (current % prime == 0)
+                {
+                    factors[prime]++;
+                    current = current / prime;
+                }
+                if (current == 1)
+                    return factors;
+            }
             return factors;
         }
     }
