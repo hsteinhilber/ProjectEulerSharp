ed19a4031059fc94f0c5d4c2b04d444682f2d4b0
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-13 08:56:59 -0400

    Start of problem 16

---
+++ Page01/Problem016.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=16
+     * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+     *
+     * What is the sum of the digits of the number 2^1000?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem016 : ProblemBase
+    {
+ 
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+
+        // protected override long ExpectedAnswer => 0;
+    }
+}
