e1e83d15b522e769d2535add48426df1f8e8d2d6
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-13 13:34:08 -0400

    initial implementation for problem 17

--- Page01/Problem017.cs
+++ Page01/Problem017.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProjectEulerSharp.Page01
@@ -20,10 +23,74 @@ namespace ProjectEulerSharp.Page01
     [TestClass]
     public class Problem017 : ProblemBase
     {
+        private readonly IList<char> LETTERS = 
+            new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private readonly IList<string> DIGITS =
+            new string[] { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", 
+                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private readonly IList<string> TENS =
+            new string[] { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
         protected override long SolutionImplementation()
         {
-            return 0;
+            return Enumerable.Range(1, 1000).Sum(i => CountLetters(i));
+        }
+
+        private int CountLetters(int i)
+        {
+            string text = NumberToText(i);
+            return text.Where(c => LETTERS.Contains(c)).Count();
+        }
+
+        // TODO: This method is limited to numbers <= 9,999 and >= 1
+        private string NumberToText(int i)
+        {
+            var thousands = i / 1000;
+            var hundreds = (i % 1000) / 100;
+            var remainder = i % 100;
+            var result = new StringBuilder();
+            
+            if (thousands > 0)
+            {
+                result.Append(DIGITS[thousands]);
+                result.Append(" thousand ");
+            }
+
+            if (hundreds > 0)
+            {
+                result.Append(DIGITS[hundreds]);
+                result.Append(" hundred ");
+            }
+
+            if ((thousands > 0 || hundreds > 0) && remainder > 0)
+                result.Append("and ");
+
+            if (remainder < 20)
+            {
+                result.Append(DIGITS[remainder]);
+            }
+            else
+            {
+                result.Append(TENS[remainder / 10]);
+                if (remainder % 10 > 0)
+                {
+                    result.Append("-");
+                    result.Append(DIGITS[remainder % 10]);
+                }
+            }
+            return result.ToString();
+        }
+
+        private string DigitToText(int c)
+        {
+
+            switch (c)
+            {
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+            }
+            return "";
         }
 
         // protected override long ExpectedAnswer => 0;
