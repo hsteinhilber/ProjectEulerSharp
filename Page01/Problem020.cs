929b8134fd1d7a6eb8c1a32c4c70e2728f9417f4
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-14 12:25:57 -0400

    added problem 20

---
+++ Page01/Problem020.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=19
+     * Title: Factorial digit sum
+     * n! means n × (n − 1) × ... × 3 × 2 × 1
+     * 
+     * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+     * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+     * 
+     * Find the sum of the digits in the number 100!
+     ******************************************************************************/
+    [TestClass]
+    public class Problem020 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0; 
+        }
+    }
+}
