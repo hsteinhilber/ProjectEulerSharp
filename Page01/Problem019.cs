319f4a47f748100ddbb53ebada6015d5c61a763e
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-14 11:34:37 -0400

    Added problem 19

---
+++ Page01/Problem019.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=19
+     * Title: Counting Sundays
+     * You are given the following information, but you may prefer to do some research for yourself.
+     * 
+     * - 1 Jan 1900 was a Monday.
+     * - Thirty days has September,
+     *   April, June and November.
+     *   All the rest have thirty-one,
+     *   Saving February alone,
+     *   Which has twenty-eight, rain or shine.
+     *   And on leap years, twenty-nine.
+     * - A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+     * 
+     * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem019 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+    }
+}
