6e76ac9fb48fd3eaece01615c75cfbbc6d9d2474
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-14 20:10:48 -0400

    New sequence class to iterate all abundant numbers

---
+++ Sequences/AbundantNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ProjectEulerSharp.Sequences
+{
+    class AbundantNumbers : IEnumerable<long>
+    {
+        private AbundantNumbers()
+        {
+
+        }
+
+        public static AbundantNumbers All => new AbundantNumbers();
+
+        IEnumerator<long> IEnumerable<long>.GetEnumerator()
+        {
+            long n = 12;
+            while (true)
+            {
+                yield return n;
+                do n++; while (!n.IsAbundant());
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<long>)this).GetEnumerator();
+        }
+    }
+}
