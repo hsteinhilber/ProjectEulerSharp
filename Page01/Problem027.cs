d992d1b435231ac62d697ce610ca6cf799787a5d
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-18 09:12:38 -0400

    added problem 27

---
+++ Page01/Problem027.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=027
+     * Title: Quadratic primes
+     * Euler discovered the remarkable quadratic formula:
+     * 
+     *          n^2 + n + 41
+     * 
+     * It turns out that the formula will produce 40 primes for the consecutive integer values 0 <= n <= 39. 
+     * However, when n=40, 40^2 +40 + 41 = 40*(40 + 1) + 41 is divisible by 41, and certainly when 
+     * n = 41, 41^2 + 41 + 41 is clearly divisible by 41.
+     * 
+     * The incredible formula n^2 - 79*n + 1601 was discovered, which produces 80 primes for the consecutive 
+     * values 0 <= n <= 79. The product of the coefficients, −79 and 1601, is −126479.
+     * 
+     * Considering quadratics of the form:
+     * 
+     *          n^2 + a*n + b, where |a| < 1000 and |b| <= 1000
+     *          
+     *          where |n| is the modulus/absolute value of n
+     *          e.g. |11| = 11 and |-4| = 4
+     *          
+     * Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum 
+     * number of primes for consecutive values of n, starting with n=0.
+     ******************************************************************************/
+    [TestClass]
+    public class Problem027 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+    }
+}
+
