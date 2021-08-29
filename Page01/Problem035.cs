c9788a6af1688a7d6db1a2efb2b9cdd92608b78b
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-28 22:18:52 -0400

    added problem 35

---
+++ Page01/Problem035.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=035
+     * Title: Circular primes
+     * The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+     * 
+     * There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+     * 
+     * How many circular primes are there below one million?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem035 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+    }
+}
+
