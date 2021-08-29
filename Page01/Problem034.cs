6e99e913999d902bbbc66163b298e1afeaf97849
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-28 22:01:07 -0400

    added problem 34

---
+++ Page01/Problem034.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=034
+     * Title: Digit factorials
+     * 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+     * 
+     * Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+     * 
+     * Note: As 1! = 1 and 2! = 2 are not sums they are not included.
+     ******************************************************************************/
+    [TestClass]
+    public class Problem034 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+    }
+}
+
