74609c0139d1950f5469d160bfce9071aeef68b7
Harry Steinhilber <harry.steinhilber@live.com>
2021-08-29 12:05:28 -0400

    added problem 36

---
+++ Page01/Problem036.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=036
+     * Title: Double-base palindromes
+     * The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+     * 
+     * Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+     * 
+     * (Please note that the palindromic number, in either base, may not include leading zeros.)
+     ******************************************************************************/
+    [TestClass]
+    public class Problem036 : ProblemBase
+    {
+        // protected override long ExpectedAnswer => 0;
+
+        protected override long SolutionImplementation()
+        {
+            return 0;
+        }
+    }
+}
+
