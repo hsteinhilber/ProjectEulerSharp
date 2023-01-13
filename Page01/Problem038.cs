cfa50a6508a89fde30c0e34772f2aa9ae26974a2
Harry Steinhilber <harry.steinhilber@live.com>
2023-01-12 22:45:35 -0500

    added Problem 38 framework

---
+++ Page01/Problem038.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using ProjectEulerSharp.Sequences;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=038
+     * Title: Pandigital Multiples
+     * Take the number 192 and multiply it by each of 1, 2, and 3:
+     * 
+     * 192 × 1 = 192
+     * 192 × 2 = 384
+     * 192 × 3 = 576
+     * By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 
+     * 192384576 the concatenated product of 192 and (1,2,3)
+     * 
+     * The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, 
+     * giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+     * 
+     * What is the largest 1 to 9 pandigital 9-digit number that can be formed as the 
+     * concatenated product of an integer with (1,2, ... , n) where n > 1?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem038 : ProblemBase
+    {
+        protected override long SolutionImplementation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
