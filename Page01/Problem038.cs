377e9f979ef6688cc3104c855aff9394c87a5619
Harry Steinhilber <harry.steinhilber@live.com>
2023-01-13 14:26:00 -0500

    24x performance improvements along with comments explaining the modifications to the initially simple loop.

--- Page01/Problem038.cs
+++ Page01/Problem038.cs
@@ -32,13 +32,52 @@ namespace ProjectEulerSharp.Page01
         protected override long SolutionImplementation()
         {
             long result = 0;
-            for (long i = 1; i <= 9999; i++)
-                for (int n = 2; n <= 9; n++)
+
+            // * For every number, the first iteration is guaranteed to produce an iteration with the same
+            //   number of digits as the value itself.
+            // * Every subsequent iteration will produce a number with the same number of digits or one more.
+            // * Numbers greater than 9999 will not be able to produce a 9-digit concatenated product as the smallest
+            //   concatenated product they can produce would contain 10-digits.
+
+
+            // Numbers 1-9 require n=5..9 to produce a 9-digit concatenated product.
+            // The initial 1-digit iteration & four 2-digit iterations can reach a 9-digit result. (n=5)
+            // The initial 1-digit iteration & eight 1-digit iterations can reach a 9-digit result. (n=9)
+            for (long i = 1; i <= 9; i++)
+                for (int n = 5; n <= 9; n++)
                 {
                     var product = i.ConcatenatedProduct(n);
                     if (product > result && product.IsPandigital())
                         result = product;
                 }
+
+            // Numbers 10-99 require n=4 to produce a 9-digit concatenated product.
+            // Three 2-digit and one 3-digit iterations are required to reach a 9-digit result.
+            for (long i = 10; i <= 99; i++)
+            {
+                var product = i.ConcatenatedProduct(4);
+                if (product > result && product.IsPandigital())
+                    result = product;
+            }
+
+            // Numbers 100-999 require n=3 to produce a 9-digit concatenated product.
+            // Three 3-digit iterations are required to reach a 9-digit result.
+            for (long i = 100; i <= 999; i++)
+            {
+                var product = i.ConcatenatedProduct(3);
+                if (product > result && product.IsPandigital())
+                    result = product;
+            }
+
+            // Numbers 1000-9999 require n=2 to produce a 9-digit concatenated product.
+            // A 4-digit iteration and a 5-digit iteration are required to reach a 9-digit result.
+            for (long i = 1000; i <= 9999; i++)
+            {
+                var product = i.ConcatenatedProduct(2);
+                if (product > result && product.IsPandigital())
+                    result = product;
+            }
+
             return result;
         }
     }
