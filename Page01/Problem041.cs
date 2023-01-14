4b32916cec0981bef673099d46d137c13e26ce93
Harry Steinhilber <harry.steinhilber@live.com>
2023-01-13 22:38:55 -0500

    Added problem 41 (skipping problem 40 for now)

---
+++ Page01/Problem041.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using ProjectEulerSharp.Sequences;
+using System.Collections.Generic;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=041
+     * Title: Pandigital Prime
+     * We shall say that an n-digit number is pandigital if it makes use of all the 
+     * digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is 
+     * also prime.
+     * 
+     * What is the largest n-digit pandigital prime that exists?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem041 : ProblemBase
+    {
+        protected override long SolutionImplementation()
+        {
+            throw new NotImplementedException();   
+        }
+    }
+}
