2c461276df4a4e436e4d86565085eb9a77a189a7
Harry Steinhilber <harry.steinhilber@live.com>
2023-01-13 21:57:23 -0500

    Added problem 39

---
+++ Page01/Problem039.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using ProjectEulerSharp.Sequences;
+using System.Collections.Generic;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=039
+     * Title: Integer Right Triangles
+     * If p is the perimeter of a right angle triangle with integral length sides, 
+     * {a,b,c}, there are exactly three solutions for p = 120.
+     * 
+     * {20,48,52}, {24,45,51}, {30,40,50}
+     * 
+     * For which value of p ≤ 1000, is the number of solutions maximised?
+     ******************************************************************************/
+    [TestClass]
+    public class Problem039 : ProblemBase
+    {
+        protected override long SolutionImplementation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
