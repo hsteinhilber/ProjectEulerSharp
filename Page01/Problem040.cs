3d0009a8b9708c615fae247eb205db3a9e7f847f
Harry Steinhilber <harry.steinhilber@live.com>
2023-01-15 09:31:27 -0500

    Added undocumented stubs for problems 40-50 that fail as inconclusive as they are not implemented.

---
+++ Page01/Problem040.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using ProjectEulerSharp.Sequences;
+using System.Collections.Generic;
+
+namespace ProjectEulerSharp.Page01
+{
+    /******************************************************************************
+     * URL: http://projecteuler.net/problem=040
+     * Title: 
+     * 
+     ******************************************************************************/
+    [TestClass]
+    public class Problem040 : ProblemBase
+    {
+        protected override long SolutionImplementation()
+        {
+            Assert.Inconclusive("Test is not yet implemented.");
+            return -1;
+        }
+    }
+}
